3015255019674312efecd17b46648b5e95f457fd
genius mu <adegbitemustapha73@gmail.com>
2025-10-18 23:42:11 +0100

    spell-commit

--- Documents/React Projects-Apps/children-learn/src/components/LearnProject.jsx
+++ Documents/React Projects-Apps/children-learn/src/components/LearnProject.jsx
@@ -11,6 +11,158 @@ const Quest = [
     question: "LION",
     options: ["L", "I", "O", "N"],
   },
+  {
+    question: "FISH",
+    options: ["F", "I", "S", "H"],
+  },
+  {
+    question: "BIRD",
+    options: ["B", "I", "R", "D"],
+  },
+  {
+    question: "FROG",
+    options: ["F", "R", "O", "G"],
+  },
+  {
+    question: "DUCK",
+    options: ["D", "U", "C", "K"],
+  },
+  {
+    question: "BEAR",
+    options: ["B", "E", "A", "R"],
+  },
+  {
+    question: "ONYX",
+    options: ["O", "N", "Y", "X"],
+  },
+  {
+    question: "ZINC",
+    options: ["Z", "I", "N", "C"],
+  },
+  {
+    question: "MUTE",
+    options: ["M", "U", "T", "E"],
+  },
+  {
+    question: "HAZE",
+    options: ["H", "A", "Z", "E"],
+  },
+  {
+    question: "WOLF",
+    options: ["W", "O", "L", "F"],
+  },
+  {
+    question: "DEER",
+    options: ["D", "E", "E", "R"],
+  },
+  {
+    question: "COW",
+    options: ["C", "O", "W"],
+  },
+  {
+    question: "DOG",
+    options: ["D", "O", "G"],
+  },
+  {
+    question: "CAT",
+    options: ["C", "A", "T"],
+  },
+  {
+    question: "VOID",
+    options: ["V", "O", "I", "D"],
+  },
+  {
+    question: "EMBER",
+    options: ["E", "M", "B", "E", "R"],
+  },
+  {
+    question: "CRYPT",
+    options: ["C", "R", "Y", "P", "T"],
+  },
+  {
+    question: "PLUSH",
+    options: ["P", "L", "U", "S", "H"],
+  },
+  {
+    question: "TWIG",
+    options: ["T", "W", "I", "G"],
+  },
+  {
+    question: "QUILL",
+    options: ["Q", "U", "I", "L", "L"],
+  },
+  {
+    question: "PIG",
+    options: ["P", "I", "G"],
+  },
+  {
+    question: "ANT",
+    options: ["A", "N", "T"],
+  },
+  {
+    question: "BAT",
+    options: ["B", "A", "T"],
+  },
+  {
+    question: "RAT",
+    options: ["R", "A", "T"],
+  },
+  {
+    question: "HEN",
+    options: ["H", "E", "N"],
+  },
+  {
+    question: "FOX",
+    options: ["F", "O", "X"],
+  },
+  {
+    question: "CROW",
+    options: ["C", "R", "O", "W"],
+  },
+  {
+    question: "SWAN",
+    options: ["S", "W", "A", "N"],
+  },
+  {
+    question: "MOON",
+    options: ["M", "O", "O", "N"],
+  },
+  {
+    question: "KING",
+    options: ["K", "I", "N", "G"],
+  },
+  {
+    question: "FIRE",
+    options: ["F", "I", "R", "E"],
+  },
+  {
+    question: "WIND",
+    options: ["W", "I", "N", "D"],
+  },
+  {
+    question: "JAZZ",
+    options: ["J", "A", "Z", "Z"],
+  },
+  {
+    question: "CODE",
+    options: ["C", "O", "D", "E"],
+  },
+  {
+    question: "QUIZ",
+    options: ["Q", "U", "I", "Z"],
+  },
+  {
+    question: "MATH",
+    options: ["M", "A", "T", "H"],
+  },
+  {
+    question: "VIBE",
+    options: ["V", "I", "B", "E"],
+  },
+  {
+    question: "RHYME",
+    options: ["R", "H", "Y", "M", "E"],
+  },
 ];
 
 const LearnProject = () => {
